8339a892fcf89932217c35a675a92334cf43c91a
hyeong-jun-kim <gamebox27@naver.com>
2022-09-29 23:37:45 +0900

    [INIT] 스프링 프로젝트 생성

---
+++ src/main/java/com/example/libertyformapiserver/LibertyFormApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.libertyformapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibertyFormApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibertyFormApiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/libertyformapiserver/LibertyFormApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libertyformapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibertyFormApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
