eca6f597eab8f7b5a0b97de56046a28d06d731f7
hyeong-jun-kim <gamebox27@naver.com>
2022-10-29 16:06:41 +0900

    TN7-148 [FEAT] 설문지 생성 Swagger 설명 추가

--- src/main/java/com/example/libertyformapiserver/controller/SurveyController.java
+++ src/main/java/com/example/libertyformapiserver/controller/SurveyController.java
@@ -5,6 +5,9 @@ import com.example.libertyformapiserver.dto.survey.create.PostCreateSurveyReq;
 import com.example.libertyformapiserver.dto.survey.create.PostCreateSurveyRes;
 import com.example.libertyformapiserver.jwt.NoIntercept;
 import com.example.libertyformapiserver.service.SurveyService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class SurveyController {
     private final SurveyService surveyService;
 
+    @ApiOperation(
+            value = "설문지 생성",
+            notes = "survey - 설문 내용, questions - 주관식, 감정, 선형대수 문항," +
+                    " choiceQuestions - 객관식 문항"
+    )
+    @ApiResponses({
+            @ApiResponse(code = 1000, message = "요청에 성공하였습니다."),
+            @ApiResponse(code = 2010, message = "존재하지 않는 유저입니다."),
+            @ApiResponse(code = 4001, message = "일치하지 않는 설문 문항입니다.")}
+    )
     @PostMapping("/create")
     @NoIntercept // TODO 추후에 제거해주기
     public BaseResponse<PostCreateSurveyRes> createSurvey(@RequestBody PostCreateSurveyReq surveyReqDto){
