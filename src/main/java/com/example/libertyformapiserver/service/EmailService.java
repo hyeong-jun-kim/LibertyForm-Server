0e4f8abf7eec10b9bcf4360acec5375a010c2f22
hyeong-jun-kim <gamebox27@naver.com>
2022-12-12 15:53:21 +0900

    [FEAT] 설문지 발송시 이메일 링크 추가

--- src/main/java/com/example/libertyformapiserver/service/EmailService.java
+++ src/main/java/com/example/libertyformapiserver/service/EmailService.java
@@ -23,24 +23,26 @@ import static com.example.libertyformapiserver.config.response.BaseResponseStatu
 @Transactional(readOnly = true)
 @Service
 public class EmailService {
+    private final String BASE_PRIVATE_URL = "http://liberty-form.shop/psurvey/";
+    private final String SUBJECT = "[LibertyForm] 설문지 발송 링크 안내입니다.";
+
     private final AmazonSimpleEmailService amazonSimpleEmailService;
 
     // 설문지 발송 링크 이메일 전송
     public void sendSurveyManagementEmail(Survey survey, List<String> receivers){
-        String subject = "[LibertyForm] 설문지 발송 링크 안내입니다.";
-        String content = getContent(survey.getCode(), survey.getExpirationDate().toString());
+        String content = getContent(survey.getName(), survey.getCode(), survey.getExpirationDate().toString());
 
-        sendSES(subject, content, receivers);
+        sendSES(SUBJECT, content, receivers);
     }
 
     // 설문 관리를 이용해서 설문 발송 이메일 보내기
     // 연락처에 있는 사람이 설문을 했는지 안했는지 추적할 수 있음
     public void sendSurveyManagementEmail(List<SurveyManagement> surveyManagements, List<String> receivers){
-        String subject = "[LibertyForm] " + "설문지 안내입니다.";
-
         for(int i = 0; i < surveyManagements.size(); i++){
             SurveyManagement surveyManagement = surveyManagements.get(i);
-            sendSES(subject, getContent(surveyManagement.getCode(), surveyManagement.getExpiredDate().toString()), Collections.singletonList(receivers.get(i)));
+
+            Survey survey = surveyManagement.getSurvey();
+            sendSES(SUBJECT, getContent(survey.getName(), survey.getCode(), surveyManagement.getExpiredDate().toString()), Collections.singletonList(receivers.get(i)));
         }
     }
 
@@ -70,32 +72,34 @@ public class EmailService {
         }
     }
 
-    // TODO 추후에 리액트 VM에 올라가면 surveyLink 반영하기
-    private String getContent(String surveyLink, String expiredDate){
-        StringBuilder emailContent = new StringBuilder();
-        emailContent.append("<!DOCTYPE html>");
-        emailContent.append("<html>");
-        emailContent.append("<head>");
-        emailContent.append("</head>");
-        emailContent.append("<body>");
-        emailContent.append("</body>");
-        emailContent.append(
+    private String getContent(String title, String code, String expiredDate){
+        StringBuilder linkSb = new StringBuilder();
+        linkSb.append(code);
+
+        StringBuilder emailContentSb = new StringBuilder();
+        emailContentSb.append("<!DOCTYPE html>");
+        emailContentSb.append("<html>");
+        emailContentSb.append("<head>");
+        emailContentSb.append("</head>");
+        emailContentSb.append("<body>");
+        emailContentSb.append("</body>");
+        emailContentSb.append(
                 " <div" +
                         "	style=\"font-family: 'Apple SD Gothic Neo', 'sans-serif' !important; width: 700px; height: 900px; border-top: 4px solid #29ABE2; margin: 100px auto; padding: 30px 0; box-sizing: border-box;\">" +
                         "	<h1 style=\"margin: 0; padding: 0 5px; font-size: 28px; font-weight: 400;\">" +
                         "		<span style=\"font-size: 50px; margin: 0 0 10px 3px;\">Liberty Form</span><br/>" +
-                        "		<span style=\"color: #29ABE2\">설문지 발송</span> 안내입니다." +
+                        "		<span style=\"color: #29ABE2\">" + title + " 설문지</span> 안내입니다." +
                         "	</h1>\n" +
                         "	<p style=\"font-size: 16px; line-height: 26px; margin-top: 50px; padding: 0 5px;\">" +
                         "	    설문 작성을 위한 링크를 알려드립니다.<br/>" +
                         "		해당 설문의 마감 기한은 " + "<strong style=\"color:#29ABE2\">"+expiredDate+"</strong>입니다. <br/>" +
-                        "   <div style=\"width: 576px;height: 90px; margin-top: 50px; padding: 0 27px;color: #242424;font-size: 16px;font-weight: bold;background-color: #F9F9F9;vertical-align: middle;line-height: 90px;\">설문지 링크 : <strong style=\"font-style: normal;font-weight: bold;color: #29ABE2\">" + surveyLink + "</strong></div>" +
+                        "   <div style=\"width: 576px;height: 90px; margin-top: 50px; padding: 0 27px;color: #242424;font-size: 16px;font-weight: bold;background-color: #F9F9F9;vertical-align: middle;line-height: 90px;\">설문지 링크 : <strong style=\"font-style: normal;font-weight: bold;color: #29ABE2\">" + linkSb + "</strong></div>" +
                         "	<p style=\"font-size: 16px; line-height: 26px; margin-top: 50px; padding: 0 5px;\">" +
                         "		감사합니다.<br/>" +
                         "	<div style=\"border-top: 4px solid #29ABE2; margin: 40px auto; padding: 30px 0;\"></div>" +
                         " </div>");
-        emailContent.append("</html>");
-        String content = emailContent.toString();
+        emailContentSb.append("</html>");
+        String content = emailContentSb.toString();
         return content;
     }
 
